0c475b038401a5657a20a0fa85bee623166ac790
Hantao Zhong <chineseiven@users.noreply.github.com>
2018-03-14 12:02:28 +0000

    游戏设置文件是否存在

--- Rianbow6_RegionSwitcher/Rianbow6_RegionSwitcher/MainWindow.xaml.cs
+++ Rianbow6_RegionSwitcher/Rianbow6_RegionSwitcher/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Rianbow6_RegionSwitcher
 {
@@ -23,6 +24,31 @@ namespace Rianbow6_RegionSwitcher
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+
+        {
+            string gameSettingFilesLocation = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\My Games\\Rainbow Six - Siege";
+            if (Directory.Exists(gameSettingFilesLocation))
+            {
+                MessageBox.Show("Rua,存在的");
+
+            }
+            else
+            {
+                MessageBox.Show("你连彩虹都没安装，真鸡儿丢人，删软件吧兄弟");
+                //TODO:丢人的没安装彩虹 强制播放一次HOP和魔王医生来进行谴责 同时背景循环机枪哥鬼畜
+            }
+
+            string[] emo = Directory.GetDirectories(gameSettingFilesLocation);
+            MessageBox.Show("发现" + emo.Length + "个垃圾玩家");
+            object[] array = Directory.GetDirectories(gameSettingFilesLocation);
+
+            //Listbbox1.Items.Add(array[0]);
+            Listbbox1.ItemsSource = array;
+
+
         }
     }
 }
